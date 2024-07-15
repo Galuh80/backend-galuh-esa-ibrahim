baef97dcdf9aaabb3c75f54ea4cabba8c46d71b1
Galuh80 <galuh.ibrahiim@gmail.com>
2024-07-15 22:19:56 +0700

    update: give permission to customer routes

--- routes/ProductRoutes.js
+++ routes/ProductRoutes.js
@@ -10,7 +10,7 @@ const merchantRoleId = parseInt(process.env.CONST_MERCHANT);
 const customerRoleId = parseInt(process.env.CONST_CUSTOMER);
 
 router.get('/products', authenticateToken, authorizeRole([merchantRoleId, customerRoleId]), productController.getAllProducts);
-router.get('/products/:id', authenticateToken, authorizeRole([merchantRoleId]), productController.getProductById);
+router.get('/products/:id', authenticateToken, authorizeRole([merchantRoleId, customerRoleId]), productController.getProductById);
 router.post('/products', authenticateToken, authorizeRole([merchantRoleId]), productController.createProduct);
 router.put('/products/:id', authenticateToken, authorizeRole([merchantRoleId]), productController.updateProductById);
 router.delete('/products/:id', authenticateToken, authorizeRole([merchantRoleId]), productController.deleteProductById);
